dbcc02c365b4d8eb54ddf09749264b2f665d5002
Chan9You <41310752+kyuchanE@users.noreply.github.com>
2026-05-17 20:53:43 +0900

    [ALL] chore: workflows > build and push ghcr

--- oss-frontend/src/app/api/health/route.ts
+++ oss-frontend/src/app/api/health/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 /**
  * Health Check
  * - 비즈니스 기능 제공이 아니라 단순하게 컨테이너가 HTTP 응답 가능한 상태인지 확인하는 진입점
- * - 위와 같은 이유로 복잡한 구조 보다는 예외적으로 얇고 단순하게 유지 ...
+ * - 위와 같은 이유로 복잡한 구조 보다는 예외적으로 얇고 단순하게 유지 ....
  * @returns 
  */
 export async function GET() {
